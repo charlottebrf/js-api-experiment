192325ae8621deb8deb2d5e542453b57eb1fc72d
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-02 10:39:48 +0000

    Corrects the test to check that the Vehicle make property is returned

--- spec/vehicle.spec.js
+++ spec/vehicle.spec.js
@@ -1,5 +1,8 @@
-const vehicle = require('../src/vehicle');
+const vehicle = require('../src/vehicle.js');
+
+jsonData = {"sixMonthRate":"","yearOfManufacture":"2009","taxDetails":"Tax due: 01 April 2018","twelveMonthRate":"","typeApproval":"M1","numberOfDoors":"5","dateOfFirstRegistration":"01 JULY 2009","colour":"SILVER","transmission":"MANUAL","fuelType":"DIESEL","wheelPlan":"2-AXLE-RIGID BODY","model":"TIGUAN SE TDI 4MOTION 140","vin":"WVGZZZ5NZAW007903","revenueWeight":"Not available","motDetails":"Expires: 28 April 2018","make":"VOLKSWAGEN","co2Emissions":"167 g/km","cylinderCapacity":"1968 cc","taxed":true,"mot":true,"taxStatus":"Tax not due"}
+vehicleInfo = new vehicle(jsonData);
 
 test('shows MOT expiry', () => {
-    expect(vehicle.make).toBe('VOLKSWAGEN');
+    expect(vehicleInfo.make).toBe('VOLKSWAGEN');
 });
--- src/vehicle.js
+++ src/vehicle.js
@@ -1,7 +1,15 @@
-function Vehicle(informationObject) {
+function vehicle(informationObject) {
     this.informationObject = informationObject;
 
     this.make = informationObject["make"];
     this.motDetails = informationObject["motDetails"];
     this.taxDetails = informationObject["taxDetails"];
 }
+
+// this.render = function() {
+//     var el = this.setup();
+//     var list = document.getElementById('headline-list');
+//     list.appendChild(el)
+// };
+
+module.exports = vehicle;
