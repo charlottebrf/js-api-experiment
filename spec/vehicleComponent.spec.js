3e573e91fde5f11194c35e52e1d52dd382532f72
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-02 11:54:08 +0000

    The Vehicle component renders the vehicle object on the page with details of: make, MOT expiry date, tax details

--- spec/vehicleComponent.spec.js
+++ spec/vehicleComponent.spec.js
@@ -3,7 +3,5 @@ const VehicleComponent = require('../src/vehicleComponent.js');
 test('vehicle component shows the make', () => {
     let vehicle = { make: 'VOLKSWAGEN'};
     let vehicleComponent = new VehicleComponent(vehicle);
-    console.log(vehicle);
-    console.log(vehicleComponent);
     expect(vehicleComponent.vehicle.make).toBe('VOLKSWAGEN');
 });
--- views/interface2.js
+++ views/interface2.js
@@ -3,37 +3,21 @@ document.addEventListener("DOMContentLoaded", function() {
     var vehicleApi = "https://dvlasearch.appspot.com/DvlaSearch?apikey=DvlaSearchDemoAccount&licencePlate=mt09nks";
 
     function getDVLAApi(api) {
-        var xmlHttp = new XMLHttpRequest();
+        let xmlHttp = new XMLHttpRequest();
         xmlHttp.open("GET", api, false);
         xmlHttp.send(null);
         return xmlHttp.responseText;
     }
 
-    var dvlaApiString = getDVLAApi(vehicleApi);
-    var dvlaApiJson = JSON.parse(dvlaApiString);
-
+    let dvlaApiString = getDVLAApi(vehicleApi);
+    let dvlaApiJson = JSON.parse(dvlaApiString);
 
     function createVehicles() {
-        var resultsHash = dvlanApiJson["response"]["results"];
-        for(var i = 0; i < resultsHash.length; i++) {
-            var newVehicle = new Vehicle(resultsHash[i])
-            var storyComponent = new StoryComponent(newVehicle);
-            storyComponent.render();
-        }
+        console.log(dvlaApiJson);
+        let newVehicle = new Vehicle(dvlaApiJson);
+        let vehicleComponent = new VehicleComponent(newVehicle);
+        vehicleComponent.render();
     }
 
-
-    function processAccountRequest() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("demo").innerHTML = this.responseText;
-            }
-        };
-        xhttp.open("POST", "https://dvlasearch.appspot.com/DvlaSearch?apikey=DvlaSearchDemoAccount&licencePlate=mt09nks", true);
-        console.log(xhttp.send());
-    }
-
-
-    createStories();
+    createVehicles();
 });
