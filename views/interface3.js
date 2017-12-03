6d64148951a73aaabd29e2f38f8f6518b6e2a187
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-03 11:36:01 +0000

    Updates API call with more modern fetch method rather than XMLHttpRequest

---
+++ views/interface3.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let vehicleApi = "https://dvlasearch.appspot.com/DvlaSearch?apikey=DvlaSearchDemoAccount&licencePlate=mt09nks";
+
+    fetch(vehicleApi)
+        .then(
+            function(response) {
+                if(response.status !== 200) {
+                    console.log("Looks like there was a problem. Status Code: " + response.status);
+                    return;
+                }
+
+                response.json().then(function(data) {
+                    let newVehicle = new Vehicle(data);
+                    let vehicleComponent = new VehicleComponent(newVehicle);
+                    vehicleComponent.render();
+                });
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error', err);
+        });
+});
